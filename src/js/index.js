7c5de094564649d64dc4e87440a719f492165909
DECOYYYYYYYYYY <a122186291@gmail.com>
2022-02-21 10:07:46 +0800

    2.21

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 const $numberOfTimeStamps = $('#numberOfTimeStamps span')
+const $numberOfTimeStamps2 = $('#numberOfTimeStamps2 span')
 const $plusBtn = $('#plusOne')
+const $subBtn = $('#leaveOne')
 const $testBtn = $('#testTimeStamps')
 const $exportBtn = $('#exportTimeStamps')
 const $clearBtn = $('#clearTimeStamps')
@@ -7,6 +9,15 @@ const $outPut = $('#outPut')
 
 const save = new Save($numberOfTimeStamps)
 const ex = new Export($outPut)
+init()
+
+function init() {
+  let sub = JSON.parse(localStorage.getItem('leave'))
+  if (!(sub instanceof Array)) {
+    sub = []
+  }
+  $numberOfTimeStamps2.html(sub.length)
+}
 
 // 振动一次30ms
 function vibrateOnce() {
@@ -17,6 +28,17 @@ $plusBtn.on('click', () => {
   vibrateOnce()
   save.addTimeStampToSave()
   save.updateLengthOfSave($numberOfTimeStamps)
+})
+
+$subBtn.on('click', () => {
+  vibrateOnce()
+  let sub = JSON.parse(localStorage.getItem('leave'))
+  if (!(sub instanceof Array)) {
+    sub = []
+  }
+  sub.push(Date.now())
+  localStorage.setItem('leave', JSON.stringify(sub))
+  $numberOfTimeStamps2.html(sub.length)
 })
 
 $testBtn.on('click', () => {
@@ -35,12 +57,21 @@ $testBtn.on('click', () => {
 
 $exportBtn.on('click', () => {
   ex.clearArr()
+  let sub = JSON.parse(localStorage.getItem('leave'))
+  if (!(sub instanceof Array)) {
+    sub = []
+  }
   ex.addItemArr(save.exportOriginData())
+  ex.addItemArr(sub)
   ex.updateUL()
 })
 
 $clearBtn.on('click', () => {
   if (confirm('确定清空吗？')) {
     save.clearSave()
+    localStorage.setItem('leave', JSON.stringify([]))
+    $numberOfTimeStamps2.html(0)
+    ex.clearUL()
   }
+
 })
